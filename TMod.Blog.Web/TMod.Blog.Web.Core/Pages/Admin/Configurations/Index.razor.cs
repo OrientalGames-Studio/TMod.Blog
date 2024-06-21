b030a7cda9f4bbeb639c11c2e3f7531f5994cdfa
源峰 郑 <13677727680@163.com>
2024-06-21 17:45:02 +0800

    配置项页 - 进行中

---
+++ TMod.Blog.Web/TMod.Blog.Web.Core/Pages/Admin/Configurations/Index.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using MudBlazor;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMod.Blog.Web.Core.Pages.Admin.Configurations
+{
+    public partial class Index:ComponentBase
+    {
+        private DateRange? _createDateRange;
+        private string? _searchConfigurationKey;
+        private IEnumerable<object>? _configurationItems;
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            _configurationItems = ["abc", "def", "ghi"];
+        }
+    }
+}
