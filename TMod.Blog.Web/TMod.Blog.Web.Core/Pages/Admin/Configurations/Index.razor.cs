78d5910544d962d0c82eb8b8c8aa7f9075807768
源峰 郑 <13677727680@163.com>
2024-06-25 18:22:10 +0800

    配置项页 - 进行中 - 数据展示

--- TMod.Blog.Web/TMod.Blog.Web.Core/Pages/Admin/Configurations/Index.razor.cs
+++ TMod.Blog.Web/TMod.Blog.Web.Core/Pages/Admin/Configurations/Index.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 using MudBlazor;
 
@@ -22,6 +23,7 @@ namespace TMod.Blog.Web.Core.Pages.Admin.Configurations
         private int _dataCount = 0;
         private int _pageCount = 1;
         private IEnumerable<ConfigurationViewModel?>? _configurationItems;
+        private HashSet<ConfigurationViewModel?>? _selectedConfigurations;
 
         [Inject]
         public IAppConfigurationProviderService? AppConfigurationProviderService { get; set; }
@@ -34,12 +36,37 @@ namespace TMod.Blog.Web.Core.Pages.Admin.Configurations
 		protected override async Task OnAfterRenderAsync(bool firstRender)
 		{
 			await base.OnAfterRenderAsync(firstRender);
-			var pagingResult = await AppConfigurationProviderService!.GetAllConfigurations(_pageSize,_pageIndex);
-			_pageIndex = pagingResult.PageIndex;
-			_pageSize = pagingResult.PageSize;
-			_dataCount = pagingResult.DataCount;
-			_pageCount = pagingResult.PageCount;
-            _configurationItems = pagingResult.Data;
 		}
+
+        private async Task<GridData<ConfigurationViewModel?>> QueryDataAsync(GridState<ConfigurationViewModel?> gridState)
+        {
+            var pagingResult = await AppConfigurationProviderService!.GetAllConfigurations(gridState.PageSize,gridState.Page,_searchConfigurationKey,_createDateRange?.Start is null?null:DateOnly.FromDateTime(_createDateRange.Start.Value),_createDateRange?.End is null?null:DateOnly.FromDateTime(_createDateRange.End.Value));
+            return new GridData<ConfigurationViewModel?>()
+            {
+                Items = pagingResult.Data,
+                TotalItems = pagingResult.DataCount
+            };
+        }
+
+        private void OnSelectedConfigurationsChanged(HashSet<ConfigurationViewModel?> selectedConfigurations)
+        {
+            _selectedConfigurations = selectedConfigurations;
+            StateHasChanged();
+        }
+
+        private void OnSearchBoxKeyDown(KeyboardEventArgs e)
+        {
+            ;
+        }
+
+        private void OnSearchBoxSearch(MouseEventArgs key)
+        {
+            return;
+        }
+
+        private void OnDateRangeChanged(DateRange dateRange)
+        {
+
+        }
 	}
 }
