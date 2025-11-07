f5d4f56e5ac369a13b1470040e50c5217e2e452f
Yuanfeng.Zheng <cn.zyf@orientalgames.cn>
2025-11-07 15:18:08 +0800

    分享链接的 Dto 定义

---
+++ src/TMod.Blog.Application/Dtos/ShareLinkDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TMod.Blog.Domain.Entities;
+
+namespace TMod.Blog.Application.Dtos
+{
+    public record ShareLinkDto
+    {
+        public int Id { get; set; }
+
+        public ShareTargetTypeEnum TargetType { get; set; }
+
+        public Guid TargetId { get; set; }
+
+        public string? ShortCode { get; set; }
+
+        public string? CreatorIp { get; set; }
+    }
+}
