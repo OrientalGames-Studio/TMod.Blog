c550dfe57aef1a09f3f62d2999677494d8d80401
Yuanfeng.Zheng <cn.zyf@orientalgames.cn>
2025-11-07 14:19:50 +0800

    标签的请求模型和 Dto 定义

---
+++ src/TMod.Blog.Application/Dtos/TagDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMod.Blog.Application.Dtos
+{
+    public record TagDto
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int ArticleCount { get; set; }
+    }
+}
